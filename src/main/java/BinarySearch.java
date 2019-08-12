c85d557253807aa45611f821435b1d43908000e9
Mike Santos <santosedinb75@gmail.com>
2019-08-11 20:34:07 -0500

    Initial Commit

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class BinarySearch {
+    BinarySearch( List<Integer> listOfUnitLength ) {
+
+    }
+
+    int indexOf( int value ) throws ValueNotFoundException {
+        int result = 0;
+        return result;
+    }
+}
