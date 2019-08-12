04e60935b5618bdcd3eefcdd65c9fb9ee1e6baf7
Mike Santos <santosedinb75@gmail.com>
2019-08-11 21:10:22 -0500

    refactor binary search

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -1,53 +1,50 @@
-//import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.List;
-//
-//class BinarySearch<T extends Comparable<? super T>> {
-//
-//    private final List<T> sortedList;
-//
-//    BinarySearch( final List<T> sortedList ) {
-//        this.sortedList = Collections.unmodifiableList( new ArrayList<T>( sortedList ) );
-//    }
-//
-//    int indexOf( T target ) throws ValueNotFoundException {
-//        return indexOf( target, 0, sortedList.size() - 1 );
-//    }
-//
-//    private int indexOf( T target, int low, int high ) throws ValueNotFoundException {
-//        if ( low > high ) {
-//            throw new ValueNotFoundException( "Value not in array" );
-//        }
-//        int mid = ( low + high ) / 2;
-//
-//        if ( target.compareTo( sortedList.get( mid ) ) == 0 ) {
-//            return mid;
-//        } else if ( target.compareTo( sortedList.get( mid ) ) < 0 ) {
-//            return indexOf( target, low, mid - 1 );
-//        } else {
-//            return indexOf( target, mid + 1, high );
-//        }
-//    }
-//}
-
 import java.util.List;
 
-public class BinarySearch {
-    private List<Integer> list;
+class BinarySearch {
+    private final List<Integer> list;
 
-    BinarySearch( List<Integer> listOfUnitLength ) {
-        this.list = listOfUnitLength;
+    BinarySearch( final List<Integer> passedList ) {
+        this.list = passedList;
     }
 
-    int indexOf( int desiredValue ) throws ValueNotFoundException {
-        int valueFoundAtIndex;
+    int indexOf( Integer target ) throws ValueNotFoundException {
+        return indexOf( target, 0, list.size() - 1 );
+    }
 
-        if ( list.contains( desiredValue ) ) {
-            valueFoundAtIndex = list.indexOf( desiredValue );
-        } else {
-            throw new ValueNotFoundException( "\"Value not in array" );
+    private int indexOf( Integer target, int low, int high ) throws ValueNotFoundException {
+        if ( low > high ) {
+            throw new ValueNotFoundException( "Value not in array" );
         }
+        int mid = ( low + high ) / 2;
 
-        return valueFoundAtIndex;
+        if ( target.compareTo( list.get( mid ) ) == 0 ) {
+            return mid;
+        } else if ( target.compareTo( list.get( mid ) ) < 0 ) {
+            return indexOf( target, low, mid - 1 );
+        } else {
+            return indexOf( target, mid + 1, high );
+        }
     }
 }
+
+//import java.util.List;
+//
+//public class BinarySearch {
+//    private List<Integer> list;
+//
+//    BinarySearch( List<Integer> listOfUnitLength ) {
+//        this.list = listOfUnitLength;
+//    }
+//
+//    int indexOf( int desiredValue ) throws ValueNotFoundException {
+//        int valueFoundAtIndex;
+//
+//        if ( list.contains( desiredValue ) ) {
+//            valueFoundAtIndex = list.indexOf( desiredValue );
+//        } else {
+//            throw new ValueNotFoundException( "\"Value not in array" );
+//        }
+//
+//        return valueFoundAtIndex;
+//    }
+//}
