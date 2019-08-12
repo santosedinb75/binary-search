47b7d07af3056a1a64e11525d5a3ed7602e885d4
Mike Santos <santosedinb75@gmail.com>
2019-08-11 21:03:52 -0500

    Non binary search solution with passing tests

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -1,31 +1,53 @@
-import java.util.ArrayList;
-import java.util.Collections;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//
+//class BinarySearch<T extends Comparable<? super T>> {
+//
+//    private final List<T> sortedList;
+//
+//    BinarySearch( final List<T> sortedList ) {
+//        this.sortedList = Collections.unmodifiableList( new ArrayList<T>( sortedList ) );
+//    }
+//
+//    int indexOf( T target ) throws ValueNotFoundException {
+//        return indexOf( target, 0, sortedList.size() - 1 );
+//    }
+//
+//    private int indexOf( T target, int low, int high ) throws ValueNotFoundException {
+//        if ( low > high ) {
+//            throw new ValueNotFoundException( "Value not in array" );
+//        }
+//        int mid = ( low + high ) / 2;
+//
+//        if ( target.compareTo( sortedList.get( mid ) ) == 0 ) {
+//            return mid;
+//        } else if ( target.compareTo( sortedList.get( mid ) ) < 0 ) {
+//            return indexOf( target, low, mid - 1 );
+//        } else {
+//            return indexOf( target, mid + 1, high );
+//        }
+//    }
+//}
+
 import java.util.List;
 
-class BinarySearch<T extends Comparable<? super T>> {
+public class BinarySearch {
+    private List<Integer> list;
 
-    private final List<T> sortedList;
-
-    BinarySearch( final List<T> sortedList ) {
-        this.sortedList = Collections.unmodifiableList( new ArrayList<T>( sortedList ) );
+    BinarySearch( List<Integer> listOfUnitLength ) {
+        this.list = listOfUnitLength;
     }
 
-    int indexOf( T target ) throws ValueNotFoundException {
-        return indexOf( target, 0, sortedList.size() - 1 );
-    }
+    int indexOf( int desiredValue ) throws ValueNotFoundException {
+        int valueFoundAtIndex;
 
-    private int indexOf( T target, int low, int high ) throws ValueNotFoundException {
-        if ( low > high ) {
-            throw new ValueNotFoundException( "Value not in array" );
-        }
-        int mid = ( low + high ) / 2;
-
-        if ( target.compareTo( sortedList.get( mid ) ) == 0 ) {
-            return mid;
-        } else if ( target.compareTo( sortedList.get( mid ) ) < 0 ) {
-            return indexOf( target, low, mid - 1 );
+        if ( list.contains( desiredValue ) ) {
+            valueFoundAtIndex = list.indexOf( desiredValue );
         } else {
-            return indexOf( target, mid + 1, high );
+            throw new ValueNotFoundException( "\"Value not in array" );
         }
+
+        return valueFoundAtIndex;
     }
 }
